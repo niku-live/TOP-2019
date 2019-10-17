0323242cecfc2a196f564b78c0c482b99957cf66
Karolis Uosis <nku2007@hotmail.com>
2019-10-17 21:07:32 +0300

    Practice 06 - added web service, logic moved to webservice

---
+++ Practice06/TestApp.Client/DataContext/WebApiDataContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TestApp.Models;
+
+namespace TestApp.DataContext
+{
+    class WebApiDataContext : IDataContext
+    {
+        public string BaseUrl { get; set; }
+
+        public WebApiDataContext(string baseUrl)
+        {
+            BaseUrl = baseUrl;
+        }
+
+        private async Task<IEnumerable<Student>> LoadFromWebServiceAsync()
+        {
+            HttpClient client = new HttpClient() { BaseAddress = new Uri(BaseUrl) };
+            var result = client.GetAsync("students").Result;
+            return await result.Content.ReadAsAsync<Student[]>();
+        }
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return LoadFromWebServiceAsync().Result;
+        }
+    }
+}
---
+++ Practice06/TestApp.Service/Controllers/StudentsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using TestApp.Models;
+
+namespace TestApp.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        public IEnumerable<Student> LoadStudents()
+        {
+            return new List<Student>()
+            {
+                new Student() { Id = 1, Name = "Jonas", CourseNo = 1},
+                new Student() { Id = 2, Name = "Petras", CourseNo = 1}
+            };
+
+        }
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return LoadStudents();
+        }
+
+        public IHttpActionResult GetStudent(int id)
+        {
+            var student = LoadStudents().FirstOrDefault((p) => p.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+    }
+}
