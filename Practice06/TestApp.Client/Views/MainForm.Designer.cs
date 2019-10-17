29f5a6c472f29fd018e6c55a735a2d4f520f0134
Karolis Uosis <nku2007@hotmail.com>
2019-10-17 21:06:20 +0300

    Practice 06 - original project without changes

---
+++ Practice06/TestApp.Client/Views/MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace TestApp.Views
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainDataGrid = new System.Windows.Forms.DataGridView();
+            this.loadDataButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mainDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainDataGrid
+            // 
+            this.mainDataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mainDataGrid.Location = new System.Drawing.Point(0, 0);
+            this.mainDataGrid.Name = "mainDataGrid";
+            this.mainDataGrid.RowHeadersWidth = 82;
+            this.mainDataGrid.RowTemplate.Height = 33;
+            this.mainDataGrid.Size = new System.Drawing.Size(803, 383);
+            this.mainDataGrid.TabIndex = 0;
+            // 
+            // loadDataButton
+            // 
+            this.loadDataButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.loadDataButton.Location = new System.Drawing.Point(629, 389);
+            this.loadDataButton.Name = "loadDataButton";
+            this.loadDataButton.Size = new System.Drawing.Size(171, 61);
+            this.loadDataButton.TabIndex = 1;
+            this.loadDataButton.Text = global::TestApp.Properties.MainResources.LoadData;
+            this.loadDataButton.UseVisualStyleBackColor = true;
+            this.loadDataButton.Click += new System.EventHandler(this.LoadDataButtonClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loadDataButton);
+            this.Controls.Add(this.mainDataGrid);
+            this.Name = "MainForm";
+            this.Text = global::TestApp.Properties.MainResources.MainForm;
+            ((System.ComponentModel.ISupportInitialize)(this.mainDataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView mainDataGrid;
+        private System.Windows.Forms.Button loadDataButton;
+    }
+}
+
