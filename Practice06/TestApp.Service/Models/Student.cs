7afe743ce726d3aa8b3ccbff6fd6d9ac243f6d68
Karolis Uosis <nku2007@hotmail.com>
2019-10-17 20:55:12 +0300

    Practice 06 - common parts moved to class libraries

--- Practice06/TestApp.Service/Models/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestApp.Models
-{
-    public class Student
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int CourseNo { get; set; }
-    }
-}
