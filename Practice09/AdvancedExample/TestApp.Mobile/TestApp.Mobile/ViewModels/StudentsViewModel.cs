7f24f29980ee31923b9961a88e9476b3fd6c60bd
Karolis Uosis <nku2007@hotmail.com>
2019-11-08 02:40:26 +0200

    Practice 09 - Advanced Mobile App. Example - Adapting previous project to mobile - Full Implementation

---
+++ Practice09/AdvancedExample/TestApp.Mobile/TestApp.Mobile/ViewModels/StudentsViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+using TestApp.Mobile.Views;
+
+namespace TestApp.Mobile.ViewModels
+{
+    public class StudentsViewModel : BaseViewModel
+    {
+        public ObservableCollection<TestApp.Models.Student> Students { get; set; }
+        public Command LoadStudentsCommand { get; set; }
+
+        public StudentsViewModel()
+        {
+            Title = "Browse";
+            Students = new ObservableCollection<TestApp.Models.Student>();
+            LoadStudentsCommand = new Command(async () => await ExecuteLoadStudentsCommand());
+
+            MessagingCenter.Subscribe<NewStudentPage, TestApp.Models.Student>(this, "AddStudent", async (obj, student) =>
+            {
+                var newStudent = student as TestApp.Models.Student;
+                Students.Add(newStudent);
+                await DataStore.AddOnUpdateStudentAsync(student);
+            });
+        }
+
+        async Task ExecuteLoadStudentsCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                Students.Clear();
+                var students = await DataStore.GetAllStudentsAsync();
+                foreach (var student in students)
+                {
+                    Students.Add(student);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
---
+++ Practice09/AdvancedExample/TestApp.Mobile/TestApp.Mobile/Views/StudentDetailPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using TestApp.Models;
+using TestApp.Mobile.ViewModels;
+
+namespace TestApp.Mobile.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class StudentDetailPage : ContentPage
+    {
+        StudentDetailViewModel viewModel;
+
+        public StudentDetailPage(StudentDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public StudentDetailPage()
+        {
+            InitializeComponent();
+
+            var student = new Student
+            {
+                Name = "Item 1",
+                CourseNo = 1
+            };
+
+            viewModel = new StudentDetailViewModel(student);
+            BindingContext = viewModel;
+        }
+    }
+}
